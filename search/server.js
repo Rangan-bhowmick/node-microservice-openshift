77e25f2cb94de4c3a9b8dbbecae4a0a84f1d6e67
Rangan-bhowmick <68549590+Rangan-bhowmick@users.noreply.github.com>
2020-08-13 21:08:06 +0530

    Update server.js

--- search/server.js
+++ search/server.js
@@ -4,6 +4,6 @@ const mongoose = require("mongoose");
 mongoose.connect(DB_URI);
 
 app.listen(3001, () => {
-  console.log("running on port 3000");
+  console.log("running on port 3001");
   console.log("--------------------------");
 });
