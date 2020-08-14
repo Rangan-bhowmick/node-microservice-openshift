d3537571e6bedfebdafaf5f9f1737fba7c3f4a12
Rangan-bhowmick <68549590+Rangan-bhowmick@users.noreply.github.com>
2020-08-14 17:26:31 +0530

    Update app.js

--- web/src/app.js
+++ web/src/app.js
@@ -3,6 +3,11 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
+  
   res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
 });
 
